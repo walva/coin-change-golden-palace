af8401a0747f5ed2663ee42c7729968154a92f4c
Ellis Benjamin <benjamin.ellis@goldenpalace.be>
2023-09-15 09:18:29 +0200

    add scenarios

--- tests/coin.test.js
+++ tests/coin.test.js
@@ -9,6 +9,46 @@ test('should pass if the amount is 31 with the return object being {five : 1 , t
     const expected = { five: 1, ten: 2, two: 3 }
     expect(change(31)).toStrictEqual(expected)
 })
+test('2', () => {
+    const expected = { two: 1, five: 0, ten: 0}
+    expect(change(2)).toStrictEqual(expected)
+})
+test('4', () => {
+    const expected = { two: 2, five: 0, ten: 0}
+    expect(change(4)).toStrictEqual(expected)
+})
+test('6', () => {
+    const expected = { two: 3, five: 0, ten: 0}
+    expect(change(6)).toStrictEqual(expected)
+})
+test('8', () => {
+    const expected = { two: 4, five: 0, ten: 0}
+    expect(change(8)).toStrictEqual(expected)
+})
+test('10', () => {
+    const expected = { two: 0, five: 0, ten: 1}
+    expect(change(10)).toStrictEqual(expected)
+})
+test('11', () => {
+    const expected = { two: 3, five: 1, ten: 0}
+    expect(change(11)).toStrictEqual(expected)
+})
+test('13', () => {
+    const expected = { two: 4, five: 1, ten: 0}
+    expect(change(13)).toStrictEqual(expected)
+})
+test('21', () => {
+    const expected = { two: 3, five: 1, ten: 1}
+    expect(change(21)).toStrictEqual(expected)
+})
+test('23', () => {
+    const expected = { two: 4, five: 1, ten: 1}
+    expect(change(23)).toStrictEqual(expected)
+})
+test('31', () => {
+    const expected = { two: 3, five: 1, ten: 2}
+    expect(change(31)).toStrictEqual(expected)
+})
 test('should pass if the amount is 9007199254740991 with the return object being {five : 1 , ten : 900719925474098 , two : 3} ', () => {
     const expected = { five: 1, ten: 900719925474098, two: 3 }
     expect(change(9007199254740991)).toStrictEqual(expected)
